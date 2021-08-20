a3b12e127b18ae5522696e71c20b0773adf49393
Nate1729 <nferreira1729@gmail.com>
2021-08-20 09:41:35 -0400

    Add find_empty_cells function and test board

--- solver/main.py
+++ solver/main.py
@@ -1,5 +1,14 @@
 """ This script solves a sudoku board using backtracking
 """
+arr_board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+             [6, 0, 0, 1, 9, 5, 0, 0, 0],
+             [0, 9, 8, 0, 0, 0, 0, 6, 0],
+             [8, 0, 0, 0, 6, 0, 0, 0, 3],
+             [4, 0, 0, 8, 0, 3, 0, 0, 1],
+             [7, 0, 0, 0, 2, 0, 0, 0, 6],
+             [0, 6, 0, 0, 0, 0, 2, 8, 0],
+             [0, 0, 0, 4, 1, 9, 0, 0, 5],
+             [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 def test_row(arr_board, row, test_num):
     """ This test if an input is valid.
@@ -10,6 +19,7 @@ def test_row(arr_board, row, test_num):
         if arr_board[row][i] == test_num:
             return False
     return True
+
 
 def test_col(arr_board, col, test_num):
     """ This tests if a n input is valid 
@@ -53,9 +63,21 @@ def test_input(arr_board, row, col, test_num):
     square_row = row // 3
     square_col = col // 3
 
-    if test_row(arr_board, row, test_num)
-        and test_col(arr_board, col, test_num)
-        and test_square(arr_board, square_row, square_col, test_num):
+    if test_row(arr_board, row, test_num) and test_col(arr_board, col, test_num) and test_square(arr_board, square_row, square_col, test_num):
         return True
     else:
         return False
+
+def find_empty_cells(arr_board):
+    """ This returns a list of tuples 
+    off all of the locations which contain 
+    a 0 in the sudoku board"""
+    ind_empty=  []
+    for row in range(9):
+        for col in range(9):
+            if arr_board[row][col] == 0:
+                ind_empty.append((row, col))
+
+    return ind_empty
+
+print(find_empty_cells(arr_board))
